ca24d38459a0ffd58406d9613d26fc081962eeb9
Vincent Mackay <mackv42@gmail.com>
2021-11-06 06:07:58 -0500

    fix dumb mistake in grid_reg

--- CV/grid.py
+++ CV/grid.py
@@ -16,19 +16,28 @@ def grid_reg(p1, p2, row, col):
 	# horizantal lines
 	for i in range(0, row):
 		y1 = p1[1]+i*y_addr
-		x1 = p1[0] > p2[0] ? p2[0] : p1[0]
-		y2 = p1[1]+i*y_addr
-		x2 = p1[0] > p2[0] ? p1[0] : p2[0]
+		x1 = p1[0]
+		y2 = p2[1]+i*y_addr
+		x2 = p2[0]
 		grid[0].push([x1, y1, x2, y2])
 
 	# vertical lines
 	for i in range(0, row):
 		x1 = p1[0]+i*x_addr
-		y1 = p1[1] > p2[1] ? p2[1] : p1[1]
-		x2 = p1[0]+i*x_addr
-		y2 = p1[1] > p2[1] ? p1[1] : p2[1]
+		y1 = p1[1]
+		x2 = p2[0]+i*x_addr
+		y2 = p2[1]
 		grid[1].push([x1, y1, x2, y2])
 
 	return grid
 
-def grid_skewed(p1, p2, p3, p4, row, col)
+def grid_rotated(p1, p2, row, col):
+	dx = p2[0] - p1[0]
+	dy = p2[1] - p1[1]
+	
+	# numerator and denominator of slope 
+	slope = [1, 1]
+
+	slope[0] = dx
+	slope[1] = dy
+
